3e39b75b8fbb61db029981b01a6ae0b7cc4d8ff0
olekhy <olekhy@gmail.com>
2014-05-09 11:46:26 +0200

    Documenting in examples

--- examples/viewmodel-mapper.php
+++ examples/viewmodel-mapper.php
@@ -5,7 +5,7 @@ use ViewModelService\ViewModelComposer;
 require __DIR__ . '/__autoloader.php';
 require __DIR__ . '/__example_classes.php';
 
-
+/** @var ExampleViewModelRepo $repo */
 $repo = ExampleViewModelRepo::getRepo();
 $repo->setViewModelComposer(new ViewModelComposer(array('namespace' => false)));
 
