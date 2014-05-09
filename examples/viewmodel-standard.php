5aa6156f23a3e29f063bb150cf635ff009dd49b1
olekhy <olekhy@gmail.com>
2014-05-09 06:21:59 +0200

    Examples added

---
+++ examples/viewmodel-standard.php
@@ -0,0 +1,21 @@
+<?php
+
+use ViewModelService\ViewModelComposer;
+
+require __DIR__ . '/__autoloader.php';
+require __DIR__ . '/__example_classes.php';
+
+
+$repo = ExampleViewModelRepo::getRepo();
+$repo->setViewModelComposer(new ViewModelComposer(array('namespace' => false)));
+
+// data from foreign resource fe database
+$callable = function()
+{
+	return array('username' => 'Monika', 'email' => 'monika@gmail.com');
+};
+
+$repo->add($callable);
+
+var_dump($repo->getUser());
+
