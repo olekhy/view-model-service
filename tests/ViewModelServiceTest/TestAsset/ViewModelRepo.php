e97d15511ecc6c48e8b16bf3bce7a32f6861e1a1
Oleksandr Khutoretskyy <olekhy@gmail.com>
2014-06-10 13:42:36 +0200

    Added new automated tests

--- tests/ViewModelServiceTest/TestAsset/ViewModelRepo.php
+++ tests/ViewModelServiceTest/TestAsset/ViewModelRepo.php
@@ -1,29 +1,41 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: al
- * Date: 6/9/14
- * Time: 2:59 PM
- */
-
 namespace ViewModelServiceTest\TestAsset;
 
 
-use ViewModelService\ViewModelRepo;
+use ViewModelService\ViewModelRepo as Repo;
 use ViewModelServiceTest\TestAsset\ViewModel\FooViewModel;
 use ViewModelServiceTest\TestAsset\ViewModel\RimmaViewModel;
 use ViewModelServiceTest\TestAsset\ViewModel\SuperViewModel;
 
 /**
- * Class ViewRepo
+ * Class ViewModelRepo
  *
  * @package ViewModelServiceTest\TestAsset
+ *
+ * - ! - View Model Register
+ * @method string addFoo(mixed $data)
+ * @method string addSuper(mixed $array)
  *
  * @method FooViewModel getFoo($optionalPostfix = null)
  * @method SuperViewModel getSuper($optionalPostfix = null)
  * @method RimmaViewModel getRimma($optionalPostfix = null)
+ *
+ * - ! - Collections register
+ *
+ * @method  collectionAddRimma()
+ *
+ * - ! - Collection pick up
+ *
+ * @method SuperViewModel[] collectionGetSuper($optionalPostfix = null)
+ * @method RimmaViewModel[] collectionGetRimma($optionalPostfix = null)
+ * @method FooViewModel[]   collectionGetFoo($optionalPostfix = null)
+ *
+ *
+ * - ! - Bad named method
+ * @method createBestEverViewModel()
+ *
+ *
  */
-class ViewRepo extends ViewModelRepo
+class ViewModelRepo extends Repo
 {
-
 }
--- tests/ViewModelServiceTest/ViewModelRepoTest.php
+++ tests/ViewModelServiceTest/ViewModelRepoTest.php
@@ -4,9 +4,12 @@ namespace ViewModelServiceTest;
 use BadMethodCallException;
 use InvalidArgumentException;
 use LogicException;
+use PHPUnit_Framework_Error;
 use PHPUnit_Framework_TestCase as TestCase;
 use ViewModelService\ViewModelComposer;
-use ViewModelService\ViewModelRepo;
+use ViewModelServiceTest\TestAsset\ViewModel\FooViewModel;
+use ViewModelServiceTest\TestAsset\ViewModelRepo;
+use ViewModelServiceTest\TestAsset\ViewModel\RimmaViewModel;
 
 class ViewModelRepoTest extends TestCase
 {
@@ -31,6 +34,151 @@ class ViewModelRepoTest extends TestCase
 	public function testShouldThrowExceptionIfInvalidRecipe()
 	{
 		$view = $this->repo->getSuper();
+	}
+
+	public function testRegisterViewModelByNameAndGetSameViewModelAsGivenOnRegister()
+	{
+		$this->repo->registerModel('Foo', 1);
+		$this->repo->registerModel('Foo', 2, 'test');
+
+		/** @var $fact1 \ViewModelServiceTest\TestAsset\ViewModel\FooViewModel */
+		$fact1 = $this->repo->getFoo();
+		/** @var $fact2 \ViewModelServiceTest\TestAsset\ViewModel\FooViewModel */
+		$fact2 = $this->repo->getFoo('test');
+
+		$this->assertInstanceOf('ViewModelServiceTest\TestAsset\ViewModel\FooViewModel', $fact1);
+		$this->assertInstanceOf(get_class($fact1), $fact2);
+
+		$this->assertSame(array(1), $fact1->getBuildArgs());
+		$this->assertSame(array(2), $fact2->getBuildArgs());
+
+		$this->assertFalse($fact1 === $fact2);
+	}
+
+	public function testShouldRegisterCollectionOfViewModelsAndGettingOfTheSameCollection()
+	{
+		$data = array(array(1), array(2), array(3));
+		$this->repo->registerModelsCollection('Rimma', $data);
+		/** @var $fact RimmaViewModel[] */
+		$fact = $this->repo->collectionGetRimma();
+
+		$this->assertTrue(3 === count($fact));
+		foreach ($fact as $key => $item)
+		{
+			$this->assertInstanceOf('ViewModelServiceTest\TestAsset\ViewModel\RimmaViewModel', $item);
+			$expectation = '0' . ++$key;
+			$this->assertSame($expectation, $item->att_1);
+		}
+	}
+
+	public function testShouldGetTheSameCollectionByUsingOfOptionalCollectionNameAddOn()
+	{
+		$data = array(
+				array('a'),
+				array('z'),
+				array('hm')
+		);
+
+		$this->repo->registerModelsCollection('Foo', $data, 'MySuperCollectionOfSuperViewModels');
+		/** @var $fact FooViewModel[] */
+		$fact = $this->repo->collectionGetFoo('MySuperCollectionOfSuperViewModels');
+
+		$this->assertTrue(3 === count($fact));
+		foreach ($fact as $key => $item)
+		{
+			$this->assertInstanceOf('ViewModelServiceTest\TestAsset\ViewModel\FooViewModel', $item);
+			$expectation = array($data[$key]);
+			$this->assertSame($expectation, $item->getBuildArgs());
+		}
+	}
+
+	/**
+	 * @expectedException InvalidArgumentException
+	 * @expectedExceptionMessage Recipe can not be attached because does not known about handling with less than one or more than two arguments
+	 */
+	public function testAddViewModelShouldThrownExceptionByInvalidArgumentsCountLessThanOne()
+	{
+			$this->repo->addFoo();
+	}
+
+	/**
+	 * @expectedException InvalidArgumentException
+	 * @expectedExceptionMessage Recipe can not be attached because does not known about handling with less than one or more than two arguments
+	 */
+	public function testAddViewModelShouldThrownExceptionByInvalidArgumentsCountMoreThanTwo()
+	{
+			$this->repo->addFoo(array(1,2,3), 'name-add-on', 'unexpected argument');
+	}
+
+	/**
+	 * @expectedException PHPUnit_Framework_Error
+	 * @expectedExceptionMessage Undefined index: Foo
+	 */
+	public function testCanNorGetTheCollectionByUsingOfBadOptionalCollectionNameAddOn()
+	{
+
+		$data = array(
+				array('lipsum')
+		);
+
+		$this->repo->registerModelsCollection('Foo', $data, 'MySuperCollectionOfSuperViewModels');
+		/** @var $fact FooViewModel[] */
+		$fact = $this->repo->collectionGetFoo();
+	}
+
+	/**
+	 * @expectedException InvalidArgumentException
+	 * @expectedExceptionMessage Sorry unexpected argument given "NULL", expected an array that each element will be used for building a ViewModel named "Rimma" in the collection
+	 */
+	public function testExceptionShouldBeThrownAtRegisterOfCollectionOfViewModelsByInvalidFittingDataNotArray_Empty()
+	{
+		$this->repo->collectionAddRimma();
+	}
+
+	/**
+	 * @expectedException InvalidArgumentException
+	 * @expectedExceptionMessage Sorry unexpected argument given "stdClass::__set_state(array(
+	))", expected an array that each element will be used for building a ViewModel named "Rimma" in the collection
+	 */
+	public function testExceptionShouldBeThrownAtRegisterOfCollectionOfViewModelsByInvalidFittingDataNotArray_InTraversableInstance()
+	{
+		$this->repo->collectionAddRimma(new \stdClass);
+	}
+
+	/**
+	 * @expectedException InvalidArgumentException
+	 * @expectedExceptionMessage Sorry unexpected argument given "NULL", expected an array that each element will be used for building a ViewModel named "Rimma" in the collection
+	 */
+	public function testExceptionShouldBeThrownAtRegisterOfCollectionOfViewModelsByInvalidFittingDataNotArray_Null()
+	{
+		$this->repo->collectionAddRimma(null);
+	}
+
+	/**
+	 * @expectedException InvalidArgumentException
+	 * @expectedExceptionMessage Sorry unexpected argument given "NULL", expected an array that each element will be used for building a ViewModel named "Rimma" in the collection
+	 */
+	public function testExceptionShouldBeThrownAtRegisterOfCollectionOfViewModelsByInvalidFittingDataNotArray_String()
+	{
+		$this->repo->collectionAddRimma('');
+	}
+
+	/**
+	 * @expectedException InvalidArgumentException
+	 * @expectedExceptionMessage Sorry unexpected argument given "123", expected an array that each element will be used for building a ViewModel named "Rimma" in the collection
+	 */
+	public function testExceptionShouldBeThrownAtRegisterOfCollectionOfViewModelsByInvalidFittingDataNotArray_Int()
+	{
+		$this->repo->collectionAddRimma(123);
+	}
+
+	/**
+	 * @expectedException InvalidArgumentException
+	 * @expectedExceptionMessage Sorry unexpected argument given "123.456", expected an array that each element will be used for building a ViewModel named "Rimma" in the collection
+	 */
+	public function testExceptionShouldBeThrownAtRegisterOfCollectionOfViewModelsByInvalidFittingDataNotArray_Real()
+	{
+		$this->repo->collectionAddRimma(123.456);
 	}
 
 	/**
@@ -86,10 +234,9 @@ class ViewModelRepoTest extends TestCase
 
 	/**
 	 * @expectedException BadMethodCallException
-	 * @expectedExceptionMessage Please use method ViewModelService\ViewModelRepo::add<NameOfView>($callable, $optionalPostfix = null) to map data to a view model
-	or ViewModelService\ViewModelRepo::get<NameOfView>($optionalPostfix = null) to get view model from repo
+	 * @expectedExceptionMessage Invalid method called "ViewModelServiceTest\TestAsset\ViewModelRepo::createBestEverViewModel()", for adding or getting ViewModel(s) only allowed "add<ViewModel>()" or "get<ViewModel>()"
 	 */
-	public function testShouldThrowExceptionWhenTryingToWorkWithMagicalCallOfAddDataOrGetViewModel()
+	public function testShouldThrowExceptionAtTryingToWorkWithMagicalMethodCallOnAddDataToRepoOrByGetViewModelFromRepo()
 	{
 			$this->repo->createBestEverViewModel();
 	}
